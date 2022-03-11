2110d31852a8776b40095c3439ba15b99737e774
bachtrananhduc <bachtrananhduc@gmail.com>
2022-03-11 16:33:02 +0700

    create-form

--- FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/user/UserController.java
+++ FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/user/UserController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rubikme.common.entity.User;
+import com.rubikme.common.entity.Role;
 
 @Controller
 public class UserController {
@@ -26,10 +29,21 @@ public class UserController {
 	@GetMapping("/users/new")
 	public String newUser(Model model) {
 		
+		List<Role> listRoles = service.listRoles();
 		User user = new User();
+		user.setEnabled(true);
 		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
 		
 		return "user_form";
 	}
 	
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		return "redirect:/users";
+	}
 }
--- FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/user/UserService.java
+++ FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/user/UserService.java
@@ -6,14 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rubikme.common.entity.User;
+import com.rubikme.common.entity.Role;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+	
+	@Autowired
+	private RoleRepository roleRepo;
 	
 	public List<User> listAll() {
-		return (List<User>) repo.findAll();
+		return (List<User>) userRepo.findAll();
+	}
+	
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();
+	}
+
+	public void save(User user) {
+		// TODO Auto-generated method stub
+		userRepo.save(user);
 	}
 }
