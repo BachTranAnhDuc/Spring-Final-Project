62b6e28874903fd6707028f719e11b5d09723e35
bachtrananhduc <bachtrananhduc@gmail.com>
2022-03-14 09:55:44 +0700

    'test-search-user'

--- FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/user/UserRepository.java
+++ FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/user/UserRepository.java
@@ -1,5 +1,7 @@
 package com.rubikme.admin.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -18,4 +20,7 @@ public interface UserRepository extends PagingAndSortingRepository<User, Integer
 	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
 	@Modifying
 	public void updateEnabledStatus(Integer id, boolean enabled);
+	
+	@Query("SELECT u FROM User u WHERE u.firstName LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
 }
