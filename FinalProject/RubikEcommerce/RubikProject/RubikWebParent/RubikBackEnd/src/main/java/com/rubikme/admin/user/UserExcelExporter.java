dc581234325f5d1158d9b1267869cc9e401803eb
bachtrananhduc <bachtrananhduc@gmail.com>
2022-03-15 09:09:09 +0700

    'clean-export-excel'

--- FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/user/UserExcelExporter.java
+++ FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/user/UserExcelExporter.java
@@ -9,7 +9,10 @@ import java.util.List;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -20,19 +23,60 @@ import org.supercsv.prefs.CsvPreference;
 import com.rubikme.common.entity.User;
 
 public class UserExcelExporter extends AbstractExporter{
+	
+	private XSSFWorkbook workbook;
+	private XSSFSheet sheet;
+	
+	public UserExcelExporter() {
+		workbook = new XSSFWorkbook();
+	}
+	
+	private void writeHeaderLine() {
+		sheet = workbook.createSheet("Users");
+		XSSFRow row = sheet.createRow(0);
+		
+		XSSFCellStyle cellStyle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setFontHeight(16);
+		cellStyle.setFont(font);
+		
+		createCell(row, 0, "User ID", cellStyle);
+		createCell(row, 1, "E-mail", cellStyle);
+		createCell(row, 2, "First Name", cellStyle);
+		createCell(row, 3, "Last Name", cellStyle);
+		createCell(row, 4, "Roles", cellStyle);
+		createCell(row, 5, "Enabled", cellStyle);
+		
+	}
+	
+	private void createCell(XSSFRow row, int columnIndex, Object value, CellStyle style) {		
+		XSSFCell cell = row.createCell(columnIndex);
+		
+		if (value instanceof Integer) {
+			cell.setCellValue((Integer) value);
+		}
+		else if (value instanceof Boolean) {
+			cell.setCellValue((Boolean) value);
+		} 
+		else {
+			cell.setCellValue((String) value);
+		}
+		
+		cell.setCellStyle(style);
+	}
+	
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
 		super.setResponseHeader(response, "application/octet-stream", ".xlsx");
+		
+		writeHeaderLine();
 		
 		
 		
 		String[] csvHeader = {"User ID", "E-mail", "First Name", "Last Name", "Roles", "Enabled"};
 		String[] fieldMapping = {"id", "email", "firstName", "lastName", "roles", "enabled"};
 		
-		XSSFWorkbook workbook = new XSSFWorkbook();
-		XSSFSheet sheet = workbook.createSheet("Users");
-		XSSFRow row = sheet.createRow(0);
-		XSSFCell cell = row.createCell(0);
-		cell.setCellValue("User ID");
+		
 		
 		ServletOutputStream outputStream = response.getOutputStream();
 		
