3243e84be7e7f3f22a1073c242e35aaa55e4f389
bachtrananhduc <bachtrananhduc@gmail.com>
2022-03-20 08:53:02 +0700

    'brand-entity'

---
+++ FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/brand/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.rubikme.admin.brand;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.rubikme.common.entity.Brand;
+
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+
+}
