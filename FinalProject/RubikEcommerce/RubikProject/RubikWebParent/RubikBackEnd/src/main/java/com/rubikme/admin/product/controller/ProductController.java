fc592eff255e2d11ce5c207768816bad452ab3cc
bachtrananhduc <bachtrananhduc@gmail.com>
2022-03-25 08:01:35 +0700

    'remove-tag'

--- FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/product/controller/ProductController.java
+++ FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.rubikme.admin.brand.BrandService;
 import com.rubikme.admin.product.ProductService;
@@ -46,5 +47,15 @@ public class ProductController {
 		model.addAttribute("pageTitle", "Create New Product");
 		
 		return "products/product_form";
+	}
+	
+	@PostMapping("/products/save")
+	public String saveProduct(Product product) {
+		
+		System.out.println("Product Name: " + product.getName());
+		System.out.println("Brand ID: " + product.getBrand().getId());
+		System.out.println("Category ID: " + product.getCategory().getId());
+		
+		return "redirect:/products";
 	}
 }
