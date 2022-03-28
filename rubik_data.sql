af55b163f3e5ecdbd1fe995389c8911056b7dae6
bachtrananhduc <bachtrananhduc@gmail.com>
2022-03-28 10:33:07 +0700

    'update-data'

--- rubik_data.sql
+++ rubik_data.sql
@@ -90,30 +90,30 @@ insert into categories values (2, 'variant_rubik', null, 1, null, 'rubik variant
 insert into categories values (3, 'accessory', null, 1, null, 'accessory', null);
 
 
-insert into categories values (4, 'rubik_2x2x2', null, 1, '2x2.jpg', '2x2x2', 1);
-insert into categories values (5, 'rubik_3x3x3', null, 0, '3x3.jpg', '3x3x3', 1);
-insert into categories values (6, 'rubik_4x4x4', null, 1, '4x4.jpg', '4x4x4', 1);
-insert into categories values (7, 'rubik_5x5x5', null, 0, '5x5.jpg', '5x5x5', 1);
-insert into categories values (8, 'rubik_6x6x6', null, 1, '6x6.png', '6x6x6', 1);
-insert into categories values (9, 'rubik_7x7x7', null, 1, '7x7.jpg', '7x7x7', 1);
-insert into categories values (10, 'rubik_8x8x8', null, 1, '8x8.jpg', '8x8x8', 1);
-insert into categories values (11, 'rubik_9x9x9', null, 1, '9x9.jpg', '9x9x9', 1);
-insert into categories values (12, 'rubik_10x10x10', null, 1, '10x10.jpg', '10x10x10', 1);
-insert into categories values (13, 'rubik_11x11x11', null, 1, '11x11.jpg', '11x11x11', 1);
-insert into categories values (14, 'rubik_12x12x12', null, 1, '12x12.jpg', '12x12x12', 1);
+insert into categories values (4, 'rubik_2x2x2', '-1-', 1, '2x2.jpg', '2x2x2', 1);
+insert into categories values (5, 'rubik_3x3x3', '-1-', 0, '3x3.jpg', '3x3x3', 1);
+insert into categories values (6, 'rubik_4x4x4', '-1-', 1, '4x4.jpg', '4x4x4', 1);
+insert into categories values (7, 'rubik_5x5x5', '-1-', 0, '5x5.jpg', '5x5x5', 1);
+insert into categories values (8, 'rubik_6x6x6', '-1-', 1, '6x6.png', '6x6x6', 1);
+insert into categories values (9, 'rubik_7x7x7', '-1-', 1, '7x7.jpg', '7x7x7', 1);
+insert into categories values (10, 'rubik_8x8x8', '-1-', 1, '8x8.jpg', '8x8x8', 1);
+insert into categories values (11, 'rubik_9x9x9', '-1-', 1, '9x9.jpg', '9x9x9', 1);
+insert into categories values (12, 'rubik_10x10x10', '-1-', 1, '10x10.jpg', '10x10x10', 1);
+insert into categories values (13, 'rubik_11x11x11', '-1-', 1, '11x11.jpg', '11x11x11', 1);
+insert into categories values (14, 'rubik_12x12x12', '-1-', 1, '12x12.jpg', '12x12x12', 1);
 
-insert into categories values (15, 'rubik_gear', null, 1, 'gear.jpg', 'Gear', 2);
-insert into categories values (16, 'rubik_megaminx', null, 1, 'megaminx.jpg', 'Megaminx', 2);
-insert into categories values (17, 'rubik_mirror', null, 1, 'mirror.jpg', 'Mirror', 2);
-insert into categories values (18, 'rubik_pyraminx', null, 1, 'pyraminx.jpg', 'Pyraminx', 2);
-insert into categories values (19, 'rubik_skewb', null, 1, 'skewb.jpg', 'Skewb', 2);
-insert into categories values (20, 'rubik_square-1', null, 1, 'square_1.jpg', 'Square-1', 2);
+insert into categories values (15, 'rubik_gear', '-2-', 1, 'gear.jpg', 'Gear', 2);
+insert into categories values (16, 'rubik_megaminx', '-2-', 1, 'megaminx.jpg', 'Megaminx', 2);
+insert into categories values (17, 'rubik_mirror', '-2-', 1, 'mirror.jpg', 'Mirror', 2);
+insert into categories values (18, 'rubik_pyraminx', '-2-', 1, 'pyraminx.jpg', 'Pyraminx', 2);
+insert into categories values (19, 'rubik_skewb', '-2-', 1, 'skewb.jpg', 'Skewb', 2);
+insert into categories values (20, 'rubik_square-1', '-2-', 1, 'square_1.jpg', 'Square-1', 2);
 
-insert into categories values (21, 'cubible_sticker', null, 1, null, 'Sticker', 3);
-insert into categories values (22, 'cubible_lube', null, 1, null, 'Lube', 3);
-insert into categories values (23, 'core', null, 1, null, 'Core', 3);
-insert into categories values (24, 'bag', null, 1, null, 'Bag', 3);
-insert into categories values (25, 'time_clock', null, 1, null, 'Time Clock', 3);
+insert into categories values (21, 'cubible_sticker', '-3-', 1, null, 'Sticker', 3);
+insert into categories values (22, 'cubible_lube', '-3-', 1, null, 'Lube', 3);
+insert into categories values (23, 'core', '-3-', 1, null, 'Core', 3);
+insert into categories values (24, 'bag', '-3-', 1, null, 'Bag', 3);
+insert into categories values (25, 'time_clock', '-3-', 1, null, 'Time Clock', 3);
 
 insert into brands values (1, 'yongjun.png', 'YongJun');
 insert into brands values (2, 'qiyi.jpg', 'QiYi');
@@ -126,26 +126,104 @@ insert into brands values (8, 'fanxin.jpg', 'Fanxin');
 insert into brands values (9, 'gocube.png', 'GoCube');
 insert into brands values (10, 'mofang.jpg', 'MoFang');
 insert into brands values (11, 'monstergo.jpg', 'Monstergo');
-insert into brands values (12, 'mscube.jpg', 'MsCube');
+insert into brands values (12, 'mscube.png', 'MsCube');
 insert into brands values (13, 'valk.jpg', 'Valk');
 insert into brands values (14, 'xmandesign.jpg', 'X-man Design');
 insert into brands values (15, 'yuxin.png', 'Yuxin');
 
-
+insert into brands_categories values (1, 4);
 insert into brands_categories values (1, 5);
-insert into brands_categories values (2, 5);
-insert into brands_categories values (3, 5);
-insert into brands_categories values (4, 5);
 insert into brands_categories values (1, 6);
+insert into brands_categories values (1, 7);
+insert into brands_categories values (1, 8);
+insert into brands_categories values (1, 9);
+insert into brands_categories values (1, 16);
+insert into brands_categories values (1, 18);
+insert into brands_categories values (1, 20);
+insert into brands_categories values (2, 4);
+insert into brands_categories values (2, 5);
 insert into brands_categories values (2, 6);
+insert into brands_categories values (2, 7);
+insert into brands_categories values (2, 8);
+insert into brands_categories values (2, 9);
+insert into brands_categories values (2, 16);
+insert into brands_categories values (2, 18);
+insert into brands_categories values (2, 19);
+insert into brands_categories values (2, 20);
+insert into brands_categories values (3, 4);
+insert into brands_categories values (3, 5);
 insert into brands_categories values (3, 6);
+insert into brands_categories values (3, 7);
+insert into brands_categories values (3, 8);
+insert into brands_categories values (3, 9);
+insert into brands_categories values (3, 18);
+insert into brands_categories values (3, 19);
+insert into brands_categories values (4, 4);
+insert into brands_categories values (4, 5);
 insert into brands_categories values (4, 6);
+insert into brands_categories values (4, 16);
+insert into brands_categories values (4, 18);
+insert into brands_categories values (4, 19);
 insert into brands_categories values (5, 4);
+insert into brands_categories values (5, 5);
+insert into brands_categories values (5, 16);
+insert into brands_categories values (5, 19);
+insert into brands_categories values (6, 5);
+insert into brands_categories values (6, 6);
+insert into brands_categories values (6, 7);
+insert into brands_categories values (6, 8);
+insert into brands_categories values (6, 9);
+insert into brands_categories values (7, 5);
+insert into brands_categories values (7, 6);
+insert into brands_categories values (7, 7);
+insert into brands_categories values (8, 5);
+insert into brands_categories values (9, 4);
+insert into brands_categories values (9, 5);
+insert into brands_categories values (10, 4);
+insert into brands_categories values (10, 5);
+insert into brands_categories values (10, 6);
+insert into brands_categories values (10, 7);
+insert into brands_categories values (10, 8);
+insert into brands_categories values (10, 9);
+insert into brands_categories values (10, 16);
+insert into brands_categories values (10, 18);
+insert into brands_categories values (10, 19);
+insert into brands_categories values (10, 20);
+insert into brands_categories values (11, 4);
+insert into brands_categories values (11, 5);
+insert into brands_categories values (11, 18);
+insert into brands_categories values (12, 5);
+insert into brands_categories values (13, 5);
+insert into brands_categories values (13, 6);
+insert into brands_categories values (13, 7);
+insert into brands_categories values (14, 4);
+insert into brands_categories values (14, 5);
+insert into brands_categories values (14, 6);
+insert into brands_categories values (14, 8);
+insert into brands_categories values (14, 9);
+insert into brands_categories values (14, 16);
+insert into brands_categories values (14, 18);
+insert into brands_categories values (14, 19);
+insert into brands_categories values (14, 20);
+insert into brands_categories values (15, 4);
+insert into brands_categories values (15, 5);
+insert into brands_categories values (15, 6);
+insert into brands_categories values (15, 7);
+insert into brands_categories values (15, 8);
+insert into brands_categories values (15, 9);
+insert into brands_categories values (15, 16);
+insert into brands_categories values (15, 18);
+insert into brands_categories values (15, 19);
+insert into brands_categories values (15, 20);
 
-insert into products values (1, 'Valk 5', 'valk-5', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
-insert into products values (2, 'Valk 6', 'valk-6', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
-insert into products values (3, 'Valk 7', 'valk-7', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
+insert into products values (1, 'YJ MGC 2x2', 'YJ_MGC_2x2', "<div><h3><b>Product Description</b></h3></div><span>The YJ MGC 2x2 is a fast and flexible magnetic 2x2 speed cube. A relatively strong magnetic feel coupled with smooth, light turning make this cube a favorite for beginners and professional solvers alike. The MGC 2x2 is&nbsp;a shining example of YJ's well-respected design philosophy: high performance, exceptional value.&nbsp;</span><div><b><br></b></div>", "<h4>Overview</h4><ul><li>Dry, fast turning</li><li>Flexible, stable mechanism</li><li>Controllable</li></ul><h4>Accessories included</h4><ul><li>Springs</li><li>Washers</li><li>Magnets</li></ul>", null, null, 1, 1, 7.00, 9.99, 5, 'main_yj_mgc_2x2_magnetic.jpg', 1, 1, 1, 1, 1, 4);
+
+insert into products values (2, 'YJ YuPo V2 Magnetic 2x2', 'YJ_YuPo_V2_Magnetic_2x2', "<div><h3><b>Product Description</b></h3></div><span>The YJ YuPo 2x2 V2 M is a fantastic budget friendly magnetic 2x2 speed cube. This puzzle turns well right out of the box and features a medium magnetic feel that is sure to please beginners and professional solvers alike.&nbsp;</span><div><b><br></b></div>", "<h4>Overview</h4><ul><li>Dry, fast turning</li><li>Flexible, stable mechanism</li><li>Controllable</li></ul><h4>Accessories included</h4><ul><li>Springs</li><li>Washers</li><li>Magnets</li></ul>", null, null, 1, 1, 5, 6.99, 5, 'YJ-YuPo-V2-Magnetic-2x2-Black_Main.jpg', 1, 1, 1, 1, 1, 4);
+
+insert into products values (3, 'YJ MGC2 Elite 2x2', 'YJ_MGC2_Elite_2x2', "<div><h3><b>Product Description</b></h3></div><span>The MGC Elite 2x2 is the second 2x2 speed cube in the MGC Elite line. It features an innovative, first of its kind on a 2x2, center/edge magnetic mechanism that offers great control and stability.&nbsp;</span><div><b><br></b></div>", "<h4>Overview</h4><ul><li></li> and eco-friendly ABS material, which mean no fading, peeling or cheating!<li></li><li></li><li></li></ul>", null, null, 1, 1, 17, 19.99, 5, 'YJ-MGC-Elite-Magnetic-2x2-Black-2-Main.jpg', 1, 1, 1, 1, 1, 4);
+
 insert into products values (4, 'Valk 8', 'valk-8', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
+
 insert into products values (5, 'Valk 9', 'valk-9', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
 insert into products values (6, 'Valk 10', 'valk-10', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
 insert into products values (7, 'Valk 11', 'valk-11', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
@@ -158,13 +236,60 @@ insert into products values (13, 'Valk 17', 'valk-17', 'some text short descript
 insert into products values (14, 'Valk 18', 'valk-18', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
 insert into products values (15, 'Valk 19', 'valk-19', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
 insert into products values (16, 'Valk 20', 'valk-20', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 5, 4);
+insert into products values (17, 'Valk 21', 'valk-21', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 1, 6);
+insert into products values (18, 'Valk 22', 'valk-22', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 2, 6);
+insert into products values (19, 'Valk 23', 'valk-23', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 3, 6);
+insert into products values (20, 'Valk 24', 'valk-24', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 4, 6);
+insert into products values (21, 'Valk 25', 'valk-25', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 1, 6);
+insert into products values (22, 'Valk 26', 'valk-26', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 2, 6);
+insert into products values (23, 'Valk 27', 'valk-27', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 3, 6);
+insert into products values (24, 'Valk 28', 'valk-28', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 4, 6);
+insert into products values (25, 'Valk 29', 'valk-29', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 1, 6);
 
-insert into products values (18, 'Valk 21', 'valk-21', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 1, 6);
-insert into products values (19, 'Valk 22', 'valk-22', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 2, 6);
-insert into products values (20, 'Valk 23', 'valk-23', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 3, 6);
-insert into products values (21, 'Valk 24', 'valk-24', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 4, 6);
-insert into products values (22, 'Valk 25', 'valk-25', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 1, 6);
-insert into products values (23, 'Valk 26', 'valk-26', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 2, 6);
-insert into products values (24, 'Valk 27', 'valk-27', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 3, 6);
-insert into products values (25, 'Valk 28', 'valk-28', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 4, 6);
-insert into products values (26, 'Valk 29', 'valk-29', 'some text short description', 'some text full description', null, null, 1, 1, 1, 100, 120, 2, 1, 1, 1, 1, 1, 6);
+insert into products values (3, 'YJ MGC2 Elite 2x2', 'YJ_MGC2_Elite_2x2', "<div><h3><b>Product Description</b></h3></div><span>&nbsp;</span><div><b><br></b></div>", "<h4>Overview</h4><ul><li></li><li></li><li></li><li></li></ul>", null, null, 1, 1, 17, 19.99, 5, 'YJ-MGC-Elite-Magnetic-2x2-Black-2-Main.jpg', 1, 1, 1, 1, 1, 4);
+
+
+-- INSERT INTO product_images
+
+-- product id = 1
+insert into product_images value (1, 'extras_yj_mgc_2x2_magnetic_1.jpg', 1);
+insert into product_images value (2, 'extras_yj_mgc_2x2_magnetic_2.jpg', 1);
+insert into product_images value (3, 'extras_yj_mgc_2x2_magnetic_3.jpg', 1);
+insert into product_images value (4, 'extras_yj_mgc_2x2_magnetic_4.jpg', 1);
+
+-- product id = 2
+insert into product_images value (1, 'YJ-YuPo-V2-Magnetic-2x2-Extras-2.jpg', 2);
+insert into product_images value (2, 'YJ-YuPo-V2-Magnetic-2x2-Stickerless-Bright-Extras-1.jpg', 2);
+
+-- product id = 3
+insert into product_images value (1, 'YJ-MGC-Elite-Magnetic-2x2-Stickerless-Bright-3-extras-1.jpg', 3);
+insert into product_images value (2, 'YJ-MGC-Elite-Magnetic-2x2-4-extras-2.jpg', 3);
+insert into product_images value (3, 'YJ-MGC-Elite-Magnetic-2x2-extras-3.jpg', 3);
+insert into product_images value (4, 'YJ-MGC-Elite-Magnetic-2x2-extras-4.jpg', 3);
+insert into product_images value (5, 'YJ-MGC-Elite-Magnetic-2x2-extras-5.jpg', 3);
+
+-- INSERT INTO product_detailsb
+
+-- product id = 1
+insert into product_details value (1, 'Brand', 'YJ', 1);
+insert into product_details value (2, 'Type', '2x2', 1);
+insert into product_details value (3, 'Magnets', 'Magnetic', 1);
+insert into product_details value (4, 'Gross Weight', '142g', 1);
+insert into product_details value (5, 'Item Weight', '66.7g', 1);
+insert into product_details value (5, 'Dimensions', '50.0mm', 1);
+
+-- product id = 2
+insert into product_details value (1, 'Brand', 'YJ', 2);
+insert into product_details value (2, 'Type', '2x2', 2);
+insert into product_details value (3, 'Magnets', 'Magnetic', 2);
+insert into product_details value (4, 'Gross Weight', '77g', 2);
+insert into product_details value (5, 'Item Weight', '65.0g', 2);
+insert into product_details value (5, 'Dimensions', '50.0mm', 2);
+
+-- product id = 3
+insert into product_details value (1, 'Brand', 'YJ', 3);
+insert into product_details value (2, 'Type', '2x2', 3);
+insert into product_details value (3, 'Magnets', 'Magnetic', 3);
+insert into product_details value (4, 'Gross Weight', '140g', 3);
+insert into product_details value (5, 'Item Weight', '65.0g', 3);
+insert into product_details value (5, 'Dimensions', '50.0mm', 3);
