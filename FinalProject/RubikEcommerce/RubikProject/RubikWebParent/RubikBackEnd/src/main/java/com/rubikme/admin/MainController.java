ddadf5c54911c98174af7f2905c62613cff1a876
bachtrananhduc <bachtrananhduc@gmail.com>
2022-04-03 17:50:03 +0700

    'hompage-back-front'

--- FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/MainController.java
+++ FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikBackEnd/src/main/java/com/rubikme/admin/MainController.java
@@ -1,5 +1,8 @@
 package com.rubikme.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,13 +10,51 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.rubikme.admin.brand.BrandService;
+import com.rubikme.admin.category.CategoryService;
+import com.rubikme.admin.product.ProductService;
+import com.rubikme.admin.user.UserService;
+import com.rubikme.common.entity.Brand;
+import com.rubikme.common.entity.Category;
+import com.rubikme.common.entity.Product;
+import com.rubikme.common.entity.User;
+
 @Controller
 public class MainController {
 	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@Autowired 
+	private BrandService brandService;
+	
+	@Autowired
+	private ProductService productService;
+	
 	@GetMapping("")
 	public String viewHomePage(Model model) {
+		
+		List<User> listUsers = userService.listAll();
+		List<Category> listCategories = categoryService.listAllCategory();
+		List<Brand> listBrands = brandService.listAll();
+		List<Product> listProducts = productService.listAll();
+		
+		Integer countUsers = listUsers.size();
+		Integer countCategories = listCategories.size();
+		Integer countBrands = listBrands.size();
+		Integer countProducts = listProducts.size();
+		
+		
 		model.addAttribute("title", "Homepage");
 		model.addAttribute("headerTitle", "/");
+		model.addAttribute("totalUsers", countUsers);
+		model.addAttribute("totalBrands", countBrands);
+		model.addAttribute("totalCategories", countCategories);
+		model.addAttribute("totalProducts", countProducts);
+		
 		return "index";
 	}
 	
