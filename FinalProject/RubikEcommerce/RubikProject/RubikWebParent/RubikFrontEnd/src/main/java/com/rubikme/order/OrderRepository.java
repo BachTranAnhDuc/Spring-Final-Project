e64e91aca3de7d5ab70c31c6e23b4adada958138
bachtrananhduc <bachtrananhduc@gmail.com>
2022-04-15 08:20:22 +0700

    manage-order-customer

--- FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikFrontEnd/src/main/java/com/rubikme/order/OrderRepository.java
+++ FinalProject/RubikEcommerce/RubikProject/RubikWebParent/RubikFrontEnd/src/main/java/com/rubikme/order/OrderRepository.java
@@ -5,13 +5,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import com.rubikme.common.entity.Customer;
 import com.rubikme.common.entity.Order;
 
 public interface OrderRepository extends PagingAndSortingRepository<Order, Integer> {
 	
-	@Query("SELECT o FROM Order o JOIN o.orderDetails od JOIN od.product p WHERE o.customer.id = ?2 AND (p.name LIKE %?1% OR o.status LIKE %?1%)")
+	@Query("SELECT DISTINCT o FROM Order o JOIN o.orderDetails od JOIN od.product p WHERE o.customer.id = ?2 AND (p.name LIKE %?1% OR o.status LIKE %?1%)")
 	public Page<Order> findAllByPageByKeyword(String keyword, Integer customerId, Pageable pageable);
 	
 	@Query("SELECT o FROM Order o WHERE o.customer.id = ?1")
 	public Page<Order> findAllByPage(Integer customerId, Pageable pageable);
+	
+	public Order findByIdAndCustomer(Integer id, Customer customer);
 }
